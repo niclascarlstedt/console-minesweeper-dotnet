145d7cb8805ca93f70ba4a10fdef726af32205ec
Niclas Carlstedt <niclas@carlstedt.com>
2023-08-31 17:50:42 +0200

    Clean Task - first commit

---
+++ Minesweeper/Minefield.cs
@@ -0,0 +1,11 @@
+﻿namespace Minesweeper;
+
+class Minefield
+{
+    private bool[,] _bombLocations = new bool[5, 5];
+
+    public void SetBomb(int x, int y)
+    {
+        _bombLocations[x, y] = true;
+    }
+}
