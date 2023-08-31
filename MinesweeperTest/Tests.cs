a23787bec2ad03f4bac6be5030b3548d01f3d3bc
Niclas Carlstedt <niclas@carlstedt.com>
2023-08-31 17:50:42 +0200

    Clean Task - first commit

---
+++ MinesweeperTest/Tests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MinesweeperTest;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
